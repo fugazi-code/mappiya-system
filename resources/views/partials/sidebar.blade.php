a9309681bc5aa439e75142dfed51e52a7f3c83f9
gero gamarcha <oreg.gamarcha@gmail.com>
2022-09-14 17:07:03 +0800

    added s in dashboard label

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -4,7 +4,7 @@
         <li class="nav-item">
             <a class="nav-link" href="{{ route('dashboard') }}">
                 <i class="icon-grid menu-icon"></i>
-                <span class="menu-title">Dashboard</span>
+                <span class="menu-title">Dashboards</span>
             </a>
         </li>
         <li class="nav-item">
