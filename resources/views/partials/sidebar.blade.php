a0d08e04a4110f7d02b6d320406b61fe63b1752a
rtrenuela <rtrenuela@objectbright.com>
2022-09-20 14:24:27 +0800

    update

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -3,84 +3,59 @@
     <ul class="nav">
         <li class="nav-item">
             <a class="nav-link" href="{{ route('dashboard') }}">
-                <i class="icon-grid menu-icon"></i>
+                <i class="menu-icon">
+                    <i class="fas fa-dashboard"></i>
+                </i>
                 <span class="menu-title">Dashboard</span>
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-                <i class="icon-layout menu-icon"></i>
+            <a class="nav-link" href="{{ route('restaurant') }}">
+                <i class="menu-icon">
+                    <i class="fas fa-business-time"></i>
+                </i>
                 <span class="menu-title">Restaurant</span>
-                <i class="menu-arrow"></i>
             </a>
-            <div class="collapse" id="ui-basic">
-                <ul class="nav flex-column sub-menu">
-                    <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Buttons</a></li>
-                    <li class="nav-item"> <a class="nav-link" href="pages/ui-features/dropdowns.html">Dropdowns</a></li>
-                    <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Typography</a></li>
-                </ul>
-            </div>
         </li>
         <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#form-elements" aria-expanded="false" aria-controls="form-elements">
-                <i class="icon-columns menu-icon"></i>
+            <a class="nav-link" href="{{ route('customer') }}">
+                <i class="menu-icon">
+                    <i class="fas fa-people-arrows"></i>
+                </i>
                 <span class="menu-title">Customer</span>
-                <i class="menu-arrow"></i>
             </a>
-            <div class="collapse" id="form-elements">
-                <ul class="nav flex-column sub-menu">
-                    <li class="nav-item"><a class="nav-link" href="pages/forms/basic_elements.html">Basic Elements</a></li>
-                </ul>
-            </div>
         </li>
         <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#charts" aria-expanded="false" aria-controls="charts">
-                <i class="icon-bar-graph menu-icon"></i>
+            <a class="nav-link" href="{{ route('delivery-people') }}">
+                <i class="menu-icon">
+                    <i class="fas fa-car"></i>
+                </i>
                 <span class="menu-title">Delivery People</span>
-                <i class="menu-arrow"></i>
             </a>
-            <div class="collapse" id="charts">
-                <ul class="nav flex-column sub-menu">
-                    <li class="nav-item"> <a class="nav-link" href="pages/charts/chartjs.html">ChartJs</a></li>
-                </ul>
-            </div>
         </li>
         <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#tables" aria-expanded="false" aria-controls="tables">
-                <i class="icon-grid-2 menu-icon"></i>
+            <a class="nav-link" href="{{ route('settings') }}">
+                <i class="menu-icon">
+                    <i class="fas fa-cogs"></i>
+                </i>
                 <span class="menu-title">Settings</span>
-                <i class="menu-arrow"></i>
             </a>
-            <div class="collapse" id="tables">
-                <ul class="nav flex-column sub-menu">
-                    <li class="nav-item"> <a class="nav-link" href="pages/tables/basic-table.html">Basic table</a></li>
-                </ul>
-            </div>
         </li>
         <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#icons" aria-expanded="false" aria-controls="icons">
-                <i class="icon-contract menu-icon"></i>
+            <a class="nav-link" href="{{ route('promocodes') }}">
+                <i class="menu-icon">
+                    <i class="fas fa-users"></i>
+                </i>
                 <span class="menu-title">Promo Codes</span>
-                <i class="menu-arrow"></i>
             </a>
-            <div class="collapse" id="icons">
-                <ul class="nav flex-column sub-menu">
-                    <li class="nav-item"> <a class="nav-link" href="pages/icons/mdi.html">Mdi icons</a></li>
-                </ul>
-            </div>
         </li>
         <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
-                <i class="icon-head menu-icon"></i>
-                <span class="menu-title">Users</span>
-                <i class="menu-arrow"></i>
+            <a class="nav-link" href="{{ route('users') }}">
+                <i class="menu-icon">
+                    <i class="fas fa-user-edit"></i>
+                </i>
+                <span class="menu-title">User</span>
             </a>
-            <div class="collapse" id="auth">
-                <ul class="nav flex-column sub-menu">
-                    <li class="nav-item"> <a class="nav-link" href="pages/samples/login.html"> Login </a></li>
-                    <li class="nav-item"> <a class="nav-link" href="pages/samples/register.html"> Register </a></li>
-                </ul>
-            </div>
         </li>
     </ul>
 </nav>
