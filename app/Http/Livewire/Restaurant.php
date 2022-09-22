74f19825039f63d253277cbc3007d1692afb78a7
blu3fire89 <blu3fire89@gmail.com>
2022-09-22 18:16:40 +0800

    update: restaurant fun submit

--- app/Http/Livewire/Restaurant.php
+++ app/Http/Livewire/Restaurant.php
@@ -2,10 +2,61 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Restaurant as RestaurantModel;
 use Livewire\Component;
 
 class Restaurant extends Component
 {
+    public $name;
+    public $address;
+    public $longitude;
+    public $latitude;
+    public $is_available;
+    public $is_blocked;
+
+    protected $attributes = [
+        'is_available' => 0,
+        'is_blocked' => 0,
+    ];
+
+    protected $rules = [
+        'name' => 'required|max:20',
+        'address' => 'required',
+        'longitude' => 'required',
+        'latitude' => 'required',
+    ];
+
+    public function updated($propertyName) {
+
+        $this->validateOnly($propertyName);
+    }
+
+    public function submit()
+    {
+        // $validatedData = $this->validate();
+        // $longitude_float = (float)$this->longitude;
+        // $latitude_float = (float)$this->latitude;
+        // $validatedData = $this->validate([
+        //     'name' => 'required',
+        //     'address' => 'required',
+        //     'longitude' => 'required',
+        //     'latitude' => 'required',
+        // ]);
+
+        RestaurantModel::create([
+            "name" => $this->name,
+            "address" => $this->address,
+            "longitude" => $this->longitude,
+            "latitude" => $this->latitude,
+            "is_available" => $this->is_available,
+            "is_blocked" => $this->is_blocked,
+        ]);
+
+        // session() -> flash('success', 'Restaurant created');
+        
+        // $this->reset();
+    }
+
     public function render()
     {
         return view('livewire.restaurant')->layout('layouts.admin');
--- resources/views/livewire/restaurant.blade.php
+++ resources/views/livewire/restaurant.blade.php
@@ -1,3 +1,33 @@
+
 <div>
     <h1>Restaurant</h1>
+    <h2>Create Restaurants:</h2>
+    
+    <form wire:submit.prevent="submit">
+        <div>
+            <label for="name">Name</label>
+            <input wire:model="name" id="name" type="text" />
+            @error('name') <div>{{ $message }}</div> @enderror
+            
+        </div>
+        <div>
+            <label for="address">Address</label>
+            <input wire:model="address" id="address" type="text" />
+            @error('address') <div>{{ $message }}</div> @enderror
+            
+        </div>
+        <div>
+            <label for="longitude">Longitude</label>
+            <input wire:model="longitude" id="longitude" type="text" />
+            @error('longitude') <div>{{ $message }}</div> @enderror
+            
+        </div>
+        <div>
+            <label for="latitude">Latitude</label>
+            <input wire:model="latitude" id="latitude" type="text" />
+            @error('latitude') <div>{{ $message }}</div> @enderror
+            
+        </div>
+        <button type="submit">Submit Me</button>
+    </form>
 </div>
