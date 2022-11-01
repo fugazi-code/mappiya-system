d1cb221a8bbf2abbdbf9afdf52741357462a0d84
blu3fire89 <blu3fire89@gmail.com>
2022-11-01 18:54:30 +0800

    feature: payment model

---
+++ database/migrations/2022_11_01_104915_create_payments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->string('payment_no');
+            $table->decimal('amount');
+            $table->string('status');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+};
